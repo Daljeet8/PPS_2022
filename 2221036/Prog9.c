1297c5656767baac0af6f9f0ccca539c637b702a
Daljeet8 <116256814+Daljeet8@users.noreply.github.com>
2022-11-10 13:26:25 +0530

    Create Prog9.c

---
+++ 2221036/Prog9.c
@@ -0,0 +1,16 @@
+### Program : Write a code to display  ASCII charater code .
+```C
+#include<stdio.h>
+int main()
+{
+char a;
+printf("Enter a character :");
+scanf("%c" ,&a);
+{
+printf(" ASCII VALUE OF %c = %d" ,a,a );
+}
+return 0;
+}
+```
+** Output: Enter a character :
+ASCII VALUE OF = **
